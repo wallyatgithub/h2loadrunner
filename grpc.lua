66ce353f3439dfb212095dcf40590bd63a2c5841
Wally Zhao <wallyzhao@gmail.com>
2022-05-19 22:06:31 +0800

    add grpc proxy lua script example

---
+++ grpc.lua
@@ -0,0 +1,18 @@
+local server_id = start_server("maock_root.json")
+
+print("server id: ", server_id)
+
+register_service_handler(server_id, "POST", "handle_request", 20)
+
+clusters = {"10.242.143.77:50051"}
+
+math.randomseed(os.time())
+
+function handle_request(response_addr, headers, payload)
+    --print ("path:", headers[":path"])
+        index = math.random(table.getn(clusters))
+        headers[":authority"] = clusters[index]
+
+    response_header, response_body, trailer = send_http_request_and_await_response(headers, payload)
+    send_response(response_addr, response_header, response_body, trailer)
+end
